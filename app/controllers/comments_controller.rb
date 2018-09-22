8d064a78ba76a24f7ef46f203a3d6068f0d15715
rhon <rhoneil.gabriel@me.com>
2018-09-22 13:01:54 +0200

    Added comment form, issue saving

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,8 @@ class CommentsController < ApplicationController
 
     def create
         @article = Article.find(params[:article_id])
-        @comment = @article.comments.create(params[:comment].permit(:email, :comment))
-        redirect_to root_path
-    end
-
-    def destroy
+        @comment = @article.comments.create(params[:comment]).permit(:email, :comment))
+        redirect_to root_path(@article)
     end
 
 end
