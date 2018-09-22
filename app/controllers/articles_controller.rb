c58f365f0a8d6fa6e5573e56dbb9fe8aa59cc475
rhon <rhoneil.gabriel@me.com>
2018-09-22 08:08:25 +0200

    Added functionality of showing created articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,22 @@
 class ArticlesController < ApplicationController
   
   def index
-    @articles = Article.all
+    @article = Article.all
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
+    if @article.save
     redirect_to root_path
+    else
+      render 'new'    
+    end
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
 
 private
 
