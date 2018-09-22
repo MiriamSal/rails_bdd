25f74a0cf236616d5c5f5a7bd92cbe58e4dd8c1d
rhon <rhoneil.gabriel@me.com>
2018-09-22 09:04:40 +0200

    Added functionality of deleting created articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,16 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
+  end
+  # # When you post a form, you do it generally with POST request as opposed to GET request. 
+  # You can think normal rails requests as GET requests, when you browse the site, if it helps.
+  # When you submit a form, the control is thrown back to the application. 
+  # How do you get the values you have submitted to the form? params is how.
+  # @article = Article.new params[:title] creates new Article to database using data of params[:title]. 
 
 private
 
